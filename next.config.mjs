eadd2c414f717fff80f3c57b7152859f1ad61844
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-12 18:26:49 -0300

    install react pwa

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,18 @@ const nextConfig = {
   reactStrictMode: true
 }
 
+const isProd = process.env.NODE_ENV === 'production'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
+
 export default nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,14 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
       </Head>
-      <link rel="shortcut icon" href="/img/icon-512.png" />
-      <link rel="apple-touch-icon" href="/img/icon-512.png" />
-      <meta
-        name="description"
-        content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-      />
       <GlobalStyles />
       <Component {...pageProps} />
     </>
